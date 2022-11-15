f638a1e7158172d593980fc82ddf15cb2ac099df
0bre4en <stanislav.kravchenko@proton.me>
2022-11-15 18:25:40 +0300

    add User model to admin panel with custom forms

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,69 @@
 from django.contrib import admin
+from .models import User
+from django import forms
+from django.contrib.auth.models import Group
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+from django.core.exceptions import ValidationError
 
-# Register your models here.
+
+class UserCreationForm(forms.ModelForm):
+    password1 = forms.CharField(label="Password", widget=forms.PasswordInput)
+    password2 = forms.CharField(
+        label="Password confirmation", widget=forms.PasswordInput
+    )
+    username = forms.CharField(label="Username", widget=forms.TextInput)
+
+    class Meta:
+        model = User
+        fields = ("email", "username")
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise ValidationError("Passwords don't match")
+        return password2
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
+
+
+class UserChangeForm(forms.ModelForm):
+    password = ReadOnlyPasswordHashField()
+
+    class Meta:
+        model = User
+        fields = ("email", "username", "password", "is_admin", "is_support")
+
+
+class UserAdmin(BaseUserAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+    list_display = ("email", "is_admin", "is_support", "username")
+    list_filter = ("is_admin", "is_support")
+    fieldsets = (
+        (None, {"fields": ("email", "username", "password")}),
+        ("Permissions", {"fields": ("is_admin", "is_support")}),
+    )
+
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "username", "password1", "password2"),
+            },
+        ),
+    )
+    search_fields = ("email", "username")
+    ordering = ("email", "username")
+    filter_horizontal = ()
+
+UserChangeForm
+admin.site.register(User, UserAdmin)
+admin.site.unregister(Group)
